c2ed2bc8e676aef8b2d93b6bc788a79fb6361082
Angelo <angelodrake@gmail.com>
2024-04-25 19:45:13 -0400

    feat(BRAT): added beta manifest and version bump variable. Changed release.yml to manual [90m<body>[39m

--- scripts/version-bump.mjs
+++ scripts/version-bump.mjs
@@ -2,20 +2,23 @@ import { readFileSync, writeFileSync } from 'fs'
 import { join } from 'path'
 
 const targetVersion = process.env.npm_package_version
+// set correct manifest file based on type of release
+const args = process.argv
+let manifestFile = args[0] === 'beta' ? 'manifest-beta.json' : 'manifest.json';
 
 console.log(`Bumping version to ${targetVersion}...`)
 
 // read minAppVersion from manifest.json and bump version to target version
-const manifest = JSON.parse(readFileSync('manifest.json', 'utf8'))
+const manifest = JSON.parse(readFileSync(manifestFile, 'utf8'))
 const { minAppVersion } = manifest
 manifest.version = targetVersion
 
 writeFileSync(
-  join('dist', 'manifest.json'),
+  join('dist', manifestFile),
   JSON.stringify(manifest, null, '\t')
 )
 
-writeFileSync('manifest.json', JSON.stringify(manifest, null, '\t'))
+writeFileSync(manifestFile, JSON.stringify(manifest, null, '\t'))
 
 // update versions.json with target version and minAppVersion from manifest.json
 const versions = JSON.parse(readFileSync('versions.json', 'utf8'))
